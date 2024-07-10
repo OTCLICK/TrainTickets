c7a7254d704cbb0a68763d66f0084c2dfafb41ed
OTCLICK <ivan.budko.2004@mail.ru>
2024-07-10 17:05:37 +0300

    Initial commit

---
+++ src/test/java/com/example/traintickets/TrainTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traintickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
